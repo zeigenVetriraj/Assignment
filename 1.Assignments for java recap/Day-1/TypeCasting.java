c4eafa08a6eb9946b5e07b68c6d62dfb2b00e040
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-19 11:07:00 +0530

    Update TypeCasting.java

--- 1.Assignments for java recap/Day-1/TypeCasting.java
+++ 1.Assignments for java recap/Day-1/TypeCasting.java
@@ -2,7 +2,6 @@
 //6. Write a program that illustrate the Explicit type casting?
 public class TypeCasting {
     public static void main(String[] args) {
-        
         double val = (double) 12 / 5; // when dividing 2 int's if the output has desimal value we have to consider
         System.out.println("Explicitly Type casted (int to double): " + val);
 
@@ -14,8 +13,14 @@ public class TypeCasting {
         // Valid casting from char to int
         char ch = 'A';
         int chVal = (int) ch; // give the ASCI value fo 'A'
-        System.out.println("Explicitly Type casted (char to int): " + chVal); 
+        System.out.println("Explicitly Type casted (char to int): " + chVal);
 
-        
+        Animal a = new Dog(); // upcasting
+        if (a instanceof Dog) {
+            Dog d = (Dog) a; // Safe downcast
+        }
     }
 }
+
+class Animal {}
+class Dog extends Animal {}
