179d65b0b6a027bdd18c58c2834a21ba3855f341
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-19 11:05:30 +0530

    Create BitOfNumber.java

---
+++ 1.Assignments for java recap/Day-1/BitOfNumber.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+//8. Write a program to find the first set bit of a number?
+public class BitOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
