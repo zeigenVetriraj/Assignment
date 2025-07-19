6590e59027027d2efe2832c9e2fedf09f74f0755
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-19 09:46:02 +0530

    Update Question1.java

--- 1.Assignments for java recap/Day-1/Question1.java
+++ 1.Assignments for java recap/Day-1/Question1.java
@@ -1,11 +1,11 @@
 
 //1. Write a code to illustrate the various scope of variables(static, local, instance, block, final variables)?
 public class Question1{
-  final double PI = 3.14159; // final variable
+  final static double PI = 3.14159; // final variable
   static String place = "Alangulam"; // static variable
   String answer; // instance variable
   
-  public static void main(Stirng[] args){
+  public static void main(String[] args){
     int x = 10; // local variable
       {
           int y = 11; // block variable
