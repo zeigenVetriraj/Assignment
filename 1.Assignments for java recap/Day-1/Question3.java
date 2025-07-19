96eca9d690748389e2840e27c7ebe1680903ac21
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-19 10:12:59 +0530

    Update Question3.java

--- 1.Assignments for java recap/Day-1/Question3.java
+++ 1.Assignments for java recap/Day-1/Question3.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 public class Question3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.print("Enter the first Number : " );
         int num1 = sc.nextInt();
+        
         System.out.print("Enter the second Number : " );
         int num2 = sc.nextInt();
         
-        System.out.println((num1>num2) ? num1 : num2); // find using ternary operator
+        // Using ternary operator to find the maximum
+        int max = (num1 > num2) ? num1 : num2;
+
+        System.out.println("The maximum of the two numbers is: " + max);
     }
 }
