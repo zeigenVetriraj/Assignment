7f89bbc2f084c4228ecab2fd5a602a199f931b14
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-19 12:15:23 +0530

    Update BitOfNumber.java

--- 1.Assignments for java recap/Day-1/BitOfNumber.java
+++ 1.Assignments for java recap/Day-1/BitOfNumber.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 public class BitOfNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.print("Enter the Number : ");
+        int num = sc.nextInt();
+        if(num%2!=0)System.out.println(1);
+        else {
+            
+        }
     }
 }
