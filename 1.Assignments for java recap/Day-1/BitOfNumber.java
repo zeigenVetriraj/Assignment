b6d2d0913ab2c897f6be5a86ab0be532f32c4b04
Zeigen Vetriraj <113341291+zeigenVetriraj@users.noreply.github.com>
2025-07-19 12:44:27 +0530

    Update BitOfNumber.java

--- 1.Assignments for java recap/Day-1/BitOfNumber.java
+++ 1.Assignments for java recap/Day-1/BitOfNumber.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 public class BitOfNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.print("Enter the Number : ");
         int num = sc.nextInt();
-        if(num%2!=0)System.out.println(1);
+        
+        if(num%2!=0)System.out.println("First bit of number is in 1");
         else {
-            
+            int count = 1;
+            while((num&1) != 1){
+                count++;
+                num = num>>1;
+            }
+            System.out.println("First bit of number is in "+count);
         }
     }
 }
